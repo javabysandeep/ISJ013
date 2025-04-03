1271ac541351da0e6005d789f2e83bdfbb7755be
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2025-04-03 11:31:31 +0530

    exception handling

---
+++ core-java/src/_13_exception_handling/Demo1.java
@@ -0,0 +1,14 @@
+package _13_exception_handling;
+
+import java.util.Scanner;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the two numbers");
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        System.out.println("Division is " + number1 / number2);
+        System.out.println("rest of the code in the main");
+    }
+}
